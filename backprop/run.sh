7f9132dd6fe32d999cf76482dcc04081f6b80d69
Nick <CoffeeBeforeArch@gmail.com>
2019-09-17 12:10:38 -0400

    Add helper bash script

---
+++ backprop/run.sh
@@ -0,0 +1,10 @@
+# Runs BackProp for a range of inputs and collects log files using nvprof
+declare -a input=(65536 131072 262144 524288)
+
+for i in ${input[@]}; do
+  nvprof --print-gpu-trace --log-file trace_"$i".txt ./backprop $i
+  nvprof --print-gpu-trace --csv --log-file trace_"$i".csv ./backprop $i
+  nvprof --metrics all --log-file metrics_"$i".txt ./backprop $i
+  nvprof --metrics all --csv --log-file metrics_"$i".csv ./backprop $i
+done
+

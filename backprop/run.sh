966dbf9ac3815f623f7b197f3a2dd2176bff4c00
Nick <CoffeeBeforeArch@gmail.com>
2019-09-18 12:09:27 -0400

    remove sm_20, get rid of warnings, collect only ipc for now

--- backprop/run.sh
+++ backprop/run.sh
@@ -4,7 +4,7 @@ declare -a input=(65536 131072 262144 524288)
 for i in ${input[@]}; do
   nvprof --print-gpu-trace --log-file trace_"$i".txt ./backprop $i
   nvprof --print-gpu-trace --csv --log-file trace_"$i".csv ./backprop $i
-  nvprof --metrics all --log-file metrics_"$i".txt ./backprop $i
-  nvprof --metrics all --csv --log-file metrics_"$i".csv ./backprop $i
+  nvprof --metrics ipc --log-file metrics_"$i".txt ./backprop $i
+  nvprof --metrics ipc --csv --log-file metrics_"$i".csv ./backprop $i
 done
 
--- gaussian/run.sh
+++ gaussian/run.sh
@@ -4,7 +4,7 @@ declare -a input=(256 512 1024 2048 4096)
 for i in ${input[@]}; do
   nvprof --print-gpu-trace --log-file trace_"$i".txt ./gaussian -s $i
   nvprof --print-gpu-trace --csv --log-file trace_"$i".csv ./gaussian -s $i
-  nvprof --metrics all --log-file metrics_"$i".txt ./gaussian -s $i
-  nvprof --metrics all --csv --log-file metrics_"$i".csv ./gaussian -s $i
+  nvprof --metrics ipc --log-file metrics_"$i".txt ./gaussian -s $i
+  nvprof --metrics ipc --csv --log-file metrics_"$i".csv ./gaussian -s $i
 done
 
